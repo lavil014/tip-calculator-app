83f7bfdde9a9ce42491d89cbde1b25882de63812
Luis Aviles <123853326+lavil014@users.noreply.github.com>
2024-11-29 22:36:13 -0600

    Updates made to script.js to add funcionality to calcultate tip

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
+const bill = 305.57;
+const tipPercentage = [5, 10, 15, 25, 50,3.5];
+const numberofPeople = 5;
 
 
+
+
+
+
+const calculateTip = ()=>{
+  let percentage = (tipPercentage[5] / 100);
+  let tipAmount = bill * percentage;
+  let totalperPerson = (bill + tipAmount) / numberofPeople;
+
+  console.log(tipAmount);
+  
+  
+  return {
+    tipAmount: tipAmount.toFixed(2),
+    totalperPerson: totalperPerson.toFixed(2)
+  }
+}
+
+console.log(calculateTip())
